822581ec24d04a7f56b8452a9bf53f01b8e4e57b
Nedick <NedelinSlavov@gmail.com>
2015-01-27 01:50:33 +0200

    Current work state

--- js/app.js
+++ js/app.js
@@ -1,3 +1,9 @@
-// Foundation JavaScript
-// Documentation can be found at: http://foundation.zurb.com/docs
 $(document).foundation();
+$(function () {
+  setTimeout(function() {
+    $("body > div:first-child").remove();
+  }, 10);
+  var locationArray = location.pathname.split('/');
+  console.log(locationArray[locationArray.length - 1]);
+  $('nav section.top-bar-section a[href^="' + locationArray[locationArray.length - 1]).parent().addClass('active');
+});

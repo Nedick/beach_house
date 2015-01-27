0814ad1e7e1aa95b82697730bcac678476ebd3a1
Nedick <NedelinSlavov@gmail.com>
2015-01-27 02:20:25 +0200

    lie russian javascript injectors :D

--- js/app.js
+++ js/app.js
@@ -1,9 +1,5 @@
 $(document).foundation();
 $(function () {
-  setTimeout(function() {
-    $("body > div:first-child").remove();
-  }, 10);
   var locationArray = location.pathname.split('/');
-  console.log(locationArray[locationArray.length - 1]);
-  $('nav section.top-bar-section a[href^="' + locationArray[locationArray.length - 1]).parent().addClass('active');
+  $('nav section.top-bar-section a[href="/' + locationArray[locationArray.length - 1]).parent().addClass('active');
 });
